6e9d7aa7e14b2cd7406b0bc619d30018142264b2
arielhalifa <arielhalifa@gmail.com>
2026-03-17 11:51:53 +0200

    second and third queries

--- part_2/queries.py
+++ part_2/queries.py
@@ -18,5 +18,36 @@ class Queries:
         return result
     
 
-    def query_2():
+    def query_2_count_signal_type():
+        cnx = Mysql.get_mysql_connection()
+        cursor = cnx.cursor()
+        # count how many signal are from every type
+        query = '''SELECT signal_type COUNT(*) FROM intel_signals
+                    GROUP BY signal_type
+                    ORDER BY COUNT(*) DESC'''
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+        cnx.close()
+        return rows
+    
+
+
+    def query_3_top_3_unknown():
+        cnx = Mysql.get_mysql_connection()
+        cursor = cnx.cursor()
+        # the top 3 targets that are unknown
+        # subquery to find all the unknown targets priority level = 99
+        query = '''SELECT entity_id, COUNT(*) FROM (SELECT * FROM targets WHERE priority_level = 99)
+                    GROUP BY entity_id
+                    ORDER BY COUNT(*) DESC
+                    LIMIT 3'''
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+        cnx.close()
+        return rows
+    
+
+    def query_4():
         pass
--- part_2/routes.py
+++ part_2/routes.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from queries import Queries
 
 app = FastAPI()
 
 
 @app.get("/q1")
 def mooving_alert():
-    pass
+    responce = Queries.query_1_mooving_alert()
+    
